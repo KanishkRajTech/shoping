e3830dcf04e0a38fb53e8c9ae43726e8d0055a89
KANISHK RAJ <kanishkraj601@gmail.com>
2025-04-12 23:12:11 +0530

    Update Cart.jsx

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import CartItem from "../components/CartItem";
+import CartItem from "../components/Cartitem";
 
 const Cart = () => {
   const { cart } = useSelector((state) => state);
